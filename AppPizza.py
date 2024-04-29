f8a732c398f2a5fd92d5350691b1b6e6562fd94a
juandavidpc14 <pirelaj197@gmail.com>
2024-04-29 00:11:17 -0400

    feat: add new functions, now the program can search clients

--- AppPizza.py
+++ AppPizza.py
@@ -38,6 +38,23 @@ def leerArchivoClientes(archivoClientes):
         
 listaClientes = leerArchivoClientes(archivoClientes)
 
+def buscarClientePorId(listaClientes, idCliente):
+    for cliente in listaClientes:
+        if cliente.idCliente == idCliente:
+            return cliente
+    return None
+        
+def encontrarUltimoId(archivoClientes):
+    ultimo_id = None
+    with open(archivoClientes, 'r', newline='', encoding='utf-8') as clientesCsv:
+        lectorCsv = csv.reader(clientesCsv, delimiter=';')
+        next(lectorCsv)  # Saltar la primera fila si contiene encabezados
+        for fila in lectorCsv:
+            id_cliente = int(fila[0])
+            if ultimo_id is None or id_cliente > ultimo_id:
+                ultimo_id = id_cliente
+    return ultimo_id
+        
 # for cliente in listaClientes: #& Estas dos lineas eran para comprobar el funcionamiento de la clase
 #     print(cliente) #& Estas dos lineas eran para comprobar el funcionamiento de la clase
 
@@ -48,8 +65,27 @@ if trabajador_cliente == 1:
     opcion_cliente = int(input("Que desea hacer?\n1.-Realizar un pedido\n2.-Salir\n"))
     while opcion_cliente != 2:
         if opcion_cliente == 1:
-            print("Funcion de realizar pedido no implementada, vuelva mas tarde!")
-            opcion_cliente = int(input("Que desea hacer?\n1.-Realizar un pedido\n2.-Salir\n"))
+            idIngreso = input("Ingrese su Id por favor: ")
+            verificarCliente = buscarClientePorId(listaClientes, idIngreso)
+            if verificarCliente:
+                print(f"Hola {verificarCliente.nombre}, cual es su pedido?")
+                break #^ Coloco break mientras tanto porque aun no se ha implementado una funcion para hacer pedido
+            else:
+                opcionRegistro = int(input("Id no encontrado, desea registrarse?\n\n1.-Si\n2.-No\n"))
+                if opcionRegistro == 1:
+                    ultimoId = encontrarUltimoId(archivoClientes)
+                    idNuevoUsuario = ultimoId + 1
+                    nombreNuevoUsuario = input("Ingrese su nombre: ")
+                    apellidoNuevoUsuario = input("Ingrese su apellido: ")
+                    emailNuevoUsuario = input("Ingrese su email: ")
+                    registroUsuario = open(archivoClientes, 'a')
+                    registroUsuario.write(f"{idNuevoUsuario};{nombreNuevoUsuario};{apellidoNuevoUsuario};{emailNuevoUsuario}\n")
+                    registroUsuario.close()
+                    print(f"\nUsted se ha registrado exitosamente, su id es: {idNuevoUsuario}")
+                    break
+                elif opcionRegistro == 2:
+                    print("Para poder realizar un pedido debe registrarse, hasta luego!!")
+                    break
 
         elif opcion_cliente == 2:
             print("Saliendo, gracias por preferirnos!!")
