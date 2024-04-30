f007736805e7cae20897f23040d9afa5b135df57
Marcial trujillo <marcialtrujillo.1234@gmail.com>
2024-04-30 09:13:15 +0200

    feat: add function to verify numbers

--- AppPizza.py
+++ AppPizza.py
@@ -24,7 +24,14 @@ class Clientes:
 
     def __str__(self):
         return(f"Id: {self.idCliente}, Nombre: {self.nombre}, Apellido: {self.apellido}, Email: {self.email} ")
-
+    
+def valor_numerico(opcion):
+    while not opcion.isdigit():
+        opcion=input("Usted no ingreso un numero\nIngrese un numero: ")
+        if opcion.isdigit():
+            return opcion
+    return opcion
+    
 def leerArchivoClientes(archivoClientes):
     listaClientes = []
     with open(archivoClientes, 'r', newline = '', encoding = 'utf-8') as clientesCsv:
@@ -59,19 +66,26 @@ def encontrarUltimoId(archivoClientes):
 #     print(cliente) #& Estas dos lineas eran para comprobar el funcionamiento de la clase
 
 print("---BIENVENIDO A LA APP OFICIAL DE LA PIZZERIA---")
-trabajador_cliente = int(input("Usted es un cliente o trabajador?\n1.-Cliente\n2.-Trabajador\n"))
-if trabajador_cliente == 1:
-    opcion_cliente = int(input("Que desea hacer?\n1.-Realizar un pedido\n2.-Salir\n"))
-    while opcion_cliente != 2:
-        if opcion_cliente == 1:
+trabajador_cliente = input("Usted es un cliente o trabajador?\n1.-Cliente\n2.-Trabajador\n")
+verificar_opcion = valor_numerico(trabajador_cliente)
+opcion_trabajador_cliente = verificar_opcion
+
+if opcion_trabajador_cliente == "1":
+    opcion_cliente = input("Que desea hacer?\n1.-Realizar un pedido\n2.-Salir\n")
+    verificar_opcion_cliente = valor_numerico(opcion_cliente)
+    opcion_verificada_cliente = verificar_opcion_cliente
+    while opcion_verificada_cliente != "2":
+        if opcion_verificada_cliente == "1":
             idIngreso = input("Ingrese su Id por favor: ")
             verificarCliente = buscarClientePorId(listaClientes, idIngreso)
             if verificarCliente:
                 print(f"Hola {verificarCliente.nombre}, cual es su pedido?")
                 break #^ Coloco break mientras tanto porque aun no se ha implementado una funcion para hacer pedido
             else:
-                opcionRegistro = int(input("Id no encontrado, desea registrarse?\n\n1.-Si\n2.-No\n"))
-                if opcionRegistro == 1:
+                opcionRegistro = input("Id no encontrado, desea registrarse?\n\n1.-Si\n2.-No\n")
+                verificar_opcion_registro = valor_numerico(opcionRegistro)
+                opcion_registro_verificada = verificar_opcion_registro
+                if opcion_registro_verificada == "1":
                     ultimoId = encontrarUltimoId(archivoClientes)
                     idNuevoUsuario = ultimoId + 1
                     nombreNuevoUsuario = input("Ingrese su nombre: ")
@@ -82,46 +96,48 @@ if trabajador_cliente == 1:
                     registroUsuario.close()
                     print(f"\nUsted se ha registrado exitosamente, su id es: {idNuevoUsuario}")
                     break
-                elif opcionRegistro == 2:
+                elif opcion_registro_verificada == "2":
                     print("Para poder realizar un pedido debe registrarse, hasta luego!!")
                     break
 
-        elif opcion_cliente == 2:
+        elif opcion_verificada_cliente == "2":
             print("Saliendo, gracias por preferirnos!!")
 
         else:
             print("Opcion invalida, ingrese nuevamente")
-            opcion_cliente = int(input("Que desea hacer?\n1.-Realizar un pedido\n2.-Salir\n"))
+            opcion_verificada_cliente = input("Que desea hacer?\n1.-Realizar un pedido\n2.-Salir\n")
 
-elif trabajador_cliente == 2:
-    opcion_trabajador = int(input("Que desea hacer el dia de hoy?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
-    while opcion_trabajador != 6:
-        if opcion_trabajador == 1:
+elif opcion_trabajador_cliente == "2":
+    opcion_trabajador = input("Que desea hacer el dia de hoy?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
+    opcion_trabajador_verificar = valor_numerico(opcion_trabajador)
+    opcion_trabajador_verificada = opcion_trabajador_verificar
+    while opcion_trabajador_verificada != "6":
+        if opcion_trabajador_verificada == "1":
             print("El capital actual es:",capital) #^ Esta opcion aun esta incompleta ya que se debe acordar como se va a guardar el capital, esto es un mero ejemplo
-            opcion_trabajador = int(input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
+            opcion_trabajador_verificada = input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
         
-        elif opcion_trabajador == 2:
+        elif opcion_trabajador_verificada == "2":
             print("Funcion aun no implementada")
-            opcion_trabajador = int(input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
+            opcion_trabajador_verificada = input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
 
-        elif opcion_trabajador == 3:
+        elif opcion_trabajador_verificada == "3":
             print("Funcion aun no implementada")
-            opcion_trabajador = int(input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
+            opcion_trabajador_verificada = input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
 
-        elif opcion_trabajador == 4:
+        elif opcion_trabajador_verificada == "4":
             print("Funcion aun no implementada")
-            opcion_trabajador = int(input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
+            opcion_trabajador_verificada = input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
 
-        elif opcion_trabajador == 5:
+        elif opcion_trabajador_verificada == "5":
             print("Funcion aun no implementada")
-            opcion_trabajador = int(input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
+            opcion_trabajador_verificada = input("Desea hacer algo mas?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
 
-        elif opcion_trabajador == 6:
+        elif opcion_trabajador_verificada == "6":
             print("Saliendo, hasta la proxima!!")
 
         else:
             print("Ingrese una opcion valida")
-            opcion_trabajador = int(input("Que desea hacer el dia de hoy?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n"))
+            opcion_trabajador_verificada = input("Que desea hacer el dia de hoy?\n1.-Mostrar el capital actual de la pizzeria\n2.-Mostrar cuanto gasto un cliente\n3.-Mostrar cuanto queda de algun ingrediente\n4.-Modificar archivo de clientes o ventas\n5.-Mostrar total de propinas\n6.-Salir\n")
 
 else:
     print("Ingrese una opcion valida por favor\n")
